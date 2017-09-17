8f77900eb9ef92345815c2661cb64c1d1a4a4e94
Yanfu Zhou <yanfu.zhou@outlook.com>
2017-09-16 21:48:43 -0500

    optimize

--- mvc/controller/flaskapi.py
+++ mvc/controller/flaskapi.py
@@ -4,7 +4,7 @@ from setting import FLASK_DEBUG
 
 log = logging.getLogger(__name__)
 
-api = Api(version="2.0 (victoria's secret)", title='Viewshed WPS',
+api = Api(version="2.0 (Victoria's secret)", title='Viewshed WPS',
           description='<a href="https://en.wikipedia.org/wiki/Viewshed">Viewshed</a> '
                       'web processing service built on top of '
                       '<a href="https://pypi.python.org/pypi/GDAL">GDAL</a> '
